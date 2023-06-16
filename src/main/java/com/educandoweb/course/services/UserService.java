8f5754de60332786f6bef12c4f1a788140b7953f
gbmira <rm95694@fiap.com.br>
2023-06-16 18:16:07 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository ur;
+	
+	public List<User> findAll(){
+		return ur.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = ur.findById(id);
+		return obj.get();
+	}
+}
